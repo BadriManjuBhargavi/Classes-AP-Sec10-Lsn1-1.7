11f1de10a459d1ac6e9af576769e859d8524c3cb
manju12345 <manju@gmail.com>
2023-07-30 14:15:31 +0530

    Intial commit

---
+++ Subject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_Sec10_Lsn1_1._7
+{
+    public class Subject
+    {
+        private string name;
+        private string shortName;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public string ShortName
+        {
+            get { return shortName; }
+            set { shortName = value; }
+        }
+    }
+}
+
+
+
+
